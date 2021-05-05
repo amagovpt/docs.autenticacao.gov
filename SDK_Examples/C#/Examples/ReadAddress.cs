8860945aa9a0d06fd5e0d7480f6d4dc5906d12bc
Tiago.Barroso <tiago.barroso@caixamagica.pt>
2021-05-05 11:43:13 +0100

    Added ReadAddress and ReadCard C# Examples

---
+++ SDK_Examples/C#/Examples/ReadAddress.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using pt.portugal.eid;
+
+namespace Examples
+{
+    class ReadAddress
+    {
+        //Main attributes needed for SDK functionalities
+        PTEID_ReaderSet readerSet = null;
+        PTEID_ReaderContext readerContext = null;
+        PTEID_EIDCard eidCard = null;
+        PTEID_EId eid = null;
+
+
+        /*
+         * Initializes the SDK and sets main variables
+         */
+        public void Initiate()
+        {
+
+            //Must always be called in the beginning of the program
+            PTEID_ReaderSet.initSDK();
+
+            //Gets the set of connected readers, if there is any inserted
+            readerSet = PTEID_ReaderSet.instance();
+            if (readerSet.readerCount() == 0)
+            {
+                throw new Exception("No Readers found!");
+            }
+
+            //Gets the first reader (index 0) and checks if there is any card inserted
+            //When multiple readers are connected, you should iterate through the various indexes
+            String readerName = readerSet.getReaderName(0);
+            readerContext = readerSet.getReaderByName(readerName);
+            if (!readerContext.isCardPresent())
+            {
+                throw new Exception("No card found in the reader!");
+            }
+
+            //Gets the card instance
+            eidCard = readerContext.getEIDCard();
+            eid = eidCard.getID();
+        }
+
+        /*
+         * Releases the SDK (must always be done at the end of the program)
+         */
+        public void Release()
+        {
+            try
+            {
+                PTEID_ReaderSet.releaseSDK();
+            }
+            catch (PTEID_Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /*
+         * Prints address info present in the card (requires address pin)
+         * @throws PTEID_Exception when there is some error with the SDK methods
+         */
+        public void ShowAddressInfo()
+        {
+
+            //The number of tries that the user has (updated with each call to verifyPin)
+            uint triesLeft = uint.MaxValue;
+
+            //Sets of the card PINs
+            PTEID_Pins pins = eidCard.getPins();
+
+            //Gets the specific PIN we want
+            //ADDR_PIN - Address Pin
+            //AUTH_PIN - Authentication Pin
+            //SIGN_PIN - Signature Pin
+            PTEID_Pin pin = pins.getPinByPinRef(PTEID_Pin.ADDR_PIN);
+
+            //If the method verifyPin is called with "" as the first argument it prompts the middleware GUI for the user to insert its PIN
+            //Otherwise we can send the PIN as the first argument and the end result will be the same
+            if (pin.verifyPin("", ref triesLeft, true)){
+
+                //SDK class that handles address related information
+                PTEID_Address address = eidCard.getAddr();
+
+               Console.WriteLine("\n\nReading address details of: " + eid.getGivenName() + " " + eid.getSurname() + ":");
+               Console.WriteLine("Country:                        " + address.getCountryCode());
+               Console.WriteLine("District:                       " + address.getDistrict());
+               Console.WriteLine("District (code):                " + address.getDistrictCode());
+               Console.WriteLine("Municipality:                   " + address.getMunicipality());
+               Console.WriteLine("Municipality (code):            " + address.getMunicipalityCode());
+               Console.WriteLine("Parish:                         " + address.getCivilParish());
+               Console.WriteLine("Parish (code):                  " + address.getCivilParishCode());
+               Console.WriteLine("Street Type (Abbreviated):      " + address.getAbbrStreetType());
+               Console.WriteLine("Street Type:                    " + address.getStreetType());
+               Console.WriteLine("Street Name:                    " + address.getStreetName());
+               Console.WriteLine("Building Type (Abbreviated):    " + address.getAbbrBuildingType());
+               Console.WriteLine("Building Type:                  " + address.getBuildingType());
+               Console.WriteLine("Door nº:                        " + address.getDoorNo());
+               Console.WriteLine("Floor:                          " + address.getFloor());
+               Console.WriteLine("Side:                           " + address.getSide ());
+               Console.WriteLine("Locality:                       " + address.getLocality());
+               Console.WriteLine("Place:                          " + address.getPlace());
+               Console.WriteLine("Postal code:                    " + address.getZip4() + "-" + address.getZip3());
+               Console.WriteLine("Postal Locality:                " + address.getPostalLocality());
+            }
+        }
+
+        public void start()
+        {
+            try
+            {
+                Initiate();
+                ShowAddressInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Release();
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new ReadAddress().start();
+        }
+    }
+}
---
+++ SDK_Examples/C#/Examples/ReadCard.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using pt.portugal.eid;
+
+namespace Examples
+{
+    class ReadCard
+    {
+        //Main attributes needed for SDK functionalities
+        PTEID_ReaderSet readerSet = null;
+        PTEID_ReaderContext readerContext = null;
+        PTEID_EIDCard eidCard = null;
+        PTEID_EId eid = null;
+        
+
+        /*
+         * Initializes the SDK and sets main variables
+         */
+        public void Initiate()
+        {
+
+            //Must always be called in the beginning of the program
+            PTEID_ReaderSet.initSDK();
+
+            //Gets the set of connected readers, if there is any inserted
+            readerSet = PTEID_ReaderSet.instance();
+            if (readerSet.readerCount() == 0) {
+                throw new Exception("No Readers found!");
+            }
+
+            //Gets the first reader (index 0) and checks if there is any card inserted
+            //When multiple readers are connected, you should iterate through the various indexes
+            String readerName = readerSet.getReaderName(0);
+            readerContext = readerSet.getReaderByName(readerName);
+            if (!readerContext.isCardPresent()) {
+                 throw new Exception("No card found in the reader!");
+            }
+
+            //Gets the card instance
+            eidCard = readerContext.getEIDCard();
+            eid = eidCard.getID();
+        }
+
+        /*
+         * Releases the SDK (must always be done at the end of the program)
+         */
+        public void Release()
+        {
+            try
+            {
+                PTEID_ReaderSet.releaseSDK();
+            }
+            catch (PTEID_Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /*
+         * Prints main info present in the card (that doesn't need address pin)
+         * @throws PTEID_Exception when there is some error with the SDK methods
+         */
+        public void ShowCardInfo()
+        {
+           Console.WriteLine("\n\nReading card details:");
+           Console.WriteLine("Name:                       " + eid.getGivenName() + " " + eid.getSurname());
+           Console.WriteLine("Card Type:                  " + eid.getDocumentType());
+           Console.WriteLine("Card Version:               " + eid.getDocumentVersion());
+           Console.WriteLine("Validaty Status:            " + eid.getValidation());
+           Console.WriteLine("Card Number:                " + eid.getDocumentNumber());
+           Console.WriteLine("Local of Request:           " + eid.getLocalofRequest());
+           Console.WriteLine("Issuing Entity:             " + eid.getIssuingEntity());
+           Console.WriteLine("Issuing Date:               " + eid.getValidityBeginDate());
+           Console.WriteLine("Validity End Date:          " + eid.getValidityEndDate());
+           Console.WriteLine("PAN Number:                 " + eid.getDocumentPAN());
+           Console.WriteLine("Civilian ID :               " + eid.getCivilianIdNumber());
+           Console.WriteLine("Tax ID:                     " + eid.getTaxNo());
+           Console.WriteLine("Social Security ID:         " + eid.getSocialSecurityNumber());
+           Console.WriteLine("Health ID:                  " + eid.getHealthNumber());
+           Console.WriteLine("Parents:                    " + eid.getParents());
+           Console.WriteLine("Father:                     " + eid.getGivenNameFather() + " " + eid.getSurnameFather());
+           Console.WriteLine("Mother:                     " + eid.getGivenNameMother() + " " + eid.getSurnameMother());
+           Console.WriteLine("Indications:                " + eid.getAccidentalIndications());
+           Console.WriteLine("Nationality:                " + eid.getNationality());
+           Console.WriteLine("Country:                    " + eid.getCountry());
+           Console.WriteLine("Date of birth:              " + eid.getDateOfBirth());
+           Console.WriteLine("Height:                     " + eid.getHeight());
+           Console.WriteLine("Gender:                     " + eid.getGender());
+           Console.WriteLine("MRZ:                        " + eid.getMRZ1());
+           Console.WriteLine("                            " + eid.getMRZ2());
+           Console.WriteLine("                            " + eid.getMRZ3());
+        }
+
+        public void start()
+        {
+            try
+            {
+                Initiate();
+                ShowCardInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                Release();
+                Console.ReadLine();
+            }
+        }   
+    
+        static void Main(string[] args)
+        {
+            new ReadCard().start();
+        }
+    }
+}
