b8923d42bf6137e3a407fdcaca7d9dad0001e417
André Guerreiro <aguerreiro1985@gmail.com>
2021-10-12 12:30:44 +0100

    Java ReadAddress: add foreign address fields

--- SDK_Examples/Java/ReadAddress.java
+++ SDK_Examples/Java/ReadAddress.java
@@ -66,9 +66,6 @@ public class ReadAddress {
         PTEID_Pins pins = eidCard.getPins();
         
         //Gets the specific PIN we want
-        //ADDR_PIN - Address Pin
-        //AUTH_PIN - Authentication Pin
-        //SIGN_PIN - Signature Pin
         PTEID_Pin pin = pins.getPinByPinRef(PTEID_Pin.ADDR_PIN);
 
         //If the method verifyPin is called with "" as the first argument it prompts the middleware GUI for the user to insert its PIN
@@ -79,25 +76,38 @@ public class ReadAddress {
             PTEID_Address address = eidCard.getAddr();
 
             System.out.println("\n\nReading address details of: " + eid.getGivenName() + " " + eid.getSurname() + ":");
-            System.out.println("Country:                        " + address.getCountryCode());
-            System.out.println("District:                       " + address.getDistrict());
-            System.out.println("District (code):                " + address.getDistrictCode());
-            System.out.println("Municipality:                   " + address.getMunicipality());
-            System.out.println("Municipality (code):            " + address.getMunicipalityCode());   
-            System.out.println("Parish:                         " + address.getCivilParish());
-            System.out.println("Parish (code):                  " + address.getCivilParishCode());
-            System.out.println("Street Type (Abbreviated):      " + address.getAbbrStreetType());
-            System.out.println("Street Type:                    " + address.getStreetType());
-            System.out.println("Street Name:                    " + address.getStreetName());
-            System.out.println("Building Type (Abbreviated):    " + address.getAbbrBuildingType());
-            System.out.println("Building Type:                  " + address.getBuildingType());
-            System.out.println("Door nº:                        " + address.getDoorNo());
-            System.out.println("Floor:                          " + address.getFloor());
-            System.out.println("Side:                           " + address.getSide ());
-            System.out.println("Locality:                       " + address.getLocality());
-            System.out.println("Place:                          " + address.getPlace());
-            System.out.println("Postal code:                    " + address.getZip4() + "-" + address.getZip3());
-            System.out.println("Postal Locality:                " + address.getPostalLocality());
+
+			if (address.isNationalAddress()) {
+				System.out.println("---National Address---");
+				System.out.println("District:                       " + address.getDistrict());
+				System.out.println("District (code):                " + address.getDistrictCode());
+				System.out.println("Municipality:                   " + address.getMunicipality());
+				System.out.println("Municipality (code):            " + address.getMunicipalityCode());
+				System.out.println("Parish:                         " + address.getCivilParish());
+				System.out.println("Parish (code):                  " + address.getCivilParishCode());
+				System.out.println("Street Type (Abbreviated):      " + address.getAbbrStreetType());
+				System.out.println("Street Type:                    " + address.getStreetType());
+				System.out.println("Street Name:                    " + address.getStreetName());
+				System.out.println("Building Type (Abbreviated):    " + address.getAbbrBuildingType());
+				System.out.println("Building Type:                  " + address.getBuildingType());
+				System.out.println("Door n:                         " + address.getDoorNo());
+				System.out.println("Floor:                          " + address.getFloor());
+				System.out.println("Side:                           " + address.getSide ());
+				System.out.println("Locality:                       " + address.getLocality());
+				System.out.println("Place:                          " + address.getPlace());
+				System.out.println("Postal code:                    " + address.getZip4() + "-" + address.getZip3());
+				System.out.println("Postal Locality:                " + address.getPostalLocality());
+				System.out.println("Gen Address:                    " + address.getGeneratedAddressCode());
+			}
+			else {
+				System.out.println("---Foreign Address---");
+				System.out.println("Address:     " + address.getForeignAddress());
+				System.out.println("City:        " + address.getForeignCity());
+				System.out.println("Locality:    " + address.getForeignLocality());
+				System.out.println("Postal Code: " + address.getForeignPostalCode());
+				System.out.println("Region:      " + address.getForeignRegion());
+				System.out.println("Country:     " + address.getForeignCountry());
+			}
         }
         
     }
