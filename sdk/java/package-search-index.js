cbbe51ec996212d2a7bee9514d113b0b982ee8db
Miguel Mota <42717736+UnlimitedPugWorks@users.noreply.github.com>
2024-02-29 17:44:42 +0000

    Update SDK Documentation for Java

--- sdk/java/package-search-index.js
+++ sdk/java/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pt.gov.cartaodecidadao"},{"l":"pteidlib"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pt.gov.cartaodecidadao"}]
