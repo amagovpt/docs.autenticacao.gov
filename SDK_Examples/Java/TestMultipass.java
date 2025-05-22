f1c658b3b1b823c42f6497735441ffb3a4961cef
André Guerreiro <aguerreiro1985@gmail.com>
2025-05-22 17:00:44 +0100

    Improve TestMultipass Java example

--- SDK_Examples/Java/TestMultipass.java
+++ SDK_Examples/Java/TestMultipass.java
@@ -1,8 +1,8 @@
 import pt.gov.cartaodecidadao.*;
 
 import java.util.stream.Collectors;
-import java.io.IOException;
 import java.util.stream.IntStream;
+import java.io.IOException;
 
 
 public class TestMultipass {
@@ -16,7 +16,6 @@ public class TestMultipass {
         }
     }
 
-    // Método para converter bytes em representação hexadecimal
     public static String bytesToHex(byte[] bytes, long size) {
         return IntStream.range(0, (int)size)
             .mapToObj(i -> String.format("%02x", bytes[i]))
@@ -25,30 +24,26 @@ public class TestMultipass {
     
     public static void main(String[] args) throws IOException, PTEID_Exception {
         try {
-            // Inicializa o SDK
             PTEID_ReaderSet.initSDK();
+            //Test mode needs test CA certificates in the appropriate location
+            //Read SDK manual for more details
             PTEID_Config.SetTestMode(true);
             
-            // Obtém o conjunto de leitores disponíveis
             PTEID_ReaderSet readerSet = PTEID_ReaderSet.instance();
-            
-            // Obtém o primeiro leitor
             PTEID_ReaderContext reader = readerSet.getReader();
             
-            // Lê o multipass token do cartão
+            // Read Multipass token data from card using contactless interface
             PTEID_ByteArray token = reader.getMultiPassToken();
             
-            // Mostra o token em formato hexadecimal
+            // Display token in hexadecimal format
             System.out.println("Token: " + bytesToHex(token.GetBytes(), token.Size()));
         }
         catch(PTEID_Exception e) {
-            // Trata erros na leitura do token
             System.err.println("Failed to read multipass token! Error: "+e.GetMessage());
             PTEID_ReaderSet.releaseSDK();
             return;
         }
         finally {
-            // Garante que os recursos são libertados mesmo em caso de erro
             PTEID_ReaderSet.releaseSDK();
         }
     }
